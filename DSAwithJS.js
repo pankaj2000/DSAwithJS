202f5125ffb288ec0e11d3002be69263ecec2d52
Pankaj Singh <68582144+pankaj2000@users.noreply.github.com>
2026-04-30 14:22:12 +0530

    Palindrome Check

--- DSAwithJS.js
+++ DSAwithJS.js
@@ -1,3 +1,12 @@
-// 2026-04-23Hello World
-console.log("Pankaj");
-console.log("Singh");
+// ** 2026-04-23--Hello World **
+// console.log("Pankaj");
+// console.log("Singh");
+
+//** 2026-04-30 - Palindrome Check **
+// function isPalindrome(num){
+//   return num<0 ? false : num === +num.toString().split('').reverse().join('')
+// }
+// const result = isPalindrome('-10');
+// console.log(result);
+
+// ** 2026-04-30 - Fibonacci Num **
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # DSAwithJS
+<!-- 2026-04-23--Hello World
+     2026-04-30 - Palindrome Check -->
