6f072190849247d4ff25a5d9bc6ddea8ff660fb2
Pankaj Singh <68582144+pankaj2000@users.noreply.github.com>
2026-05-01 15:26:34 +0530

    Anagram

--- DSAwithJS.js
+++ DSAwithJS.js
@@ -31,3 +31,27 @@
 // }
 
 // console.log(fib(5))
+
+// ==============================
+// 📅 2026-05-01 — Anagram
+// ==============================
+
+// var anagram = function(a,b){
+//     if(a.length != b.length) return false;
+
+//     let obj1 = {};
+//     let obj2 = {};
+
+//     for(var i = 0; i< a.length; i++){
+//         obj1[a[i]] = (obj1[a[i]] || 0) +1;
+//          obj2[b[i]] = (obj2[b[i]] || 0) +1;
+//     }
+
+//     for(const key in obj1){
+//         if(obj1[key] !== obj2[key]) return false;
+//     }
+
+//     return true;
+// }
+
+// console.log(anagram('pankaj','jknpak'))
